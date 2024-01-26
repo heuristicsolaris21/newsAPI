c85b11971d1998722065b3cb1cf6f0440050dfa2
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2024-01-26 15:19:20 +0530

    added api url

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ const app= express();
 app.use(express.json());
 
 app.use(cors());
+app.use(
+	cors({
+		// origin: "http://localhost:3000",
+		origin: "https://kovai-news-api.onrender.com",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
 
 function getTodayDate() {
     const today = new Date();
